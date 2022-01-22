8d572bdb02055a90623c2c612ee3018d52c68848
Jordi <damlabulbul26@gmail.com>
2022-01-21 20:49:46 -0800

    more progress on css and js

--- assets/script.js
+++ assets/script.js
@@ -105,4 +105,28 @@ timeBlockData.forEach(function(timeBlockEl, index) {
         var hourEl = $("<div>")
          .text(`${timeBlockEl.hour}${timeBlockEl.meridiem}`)
          hourEl.addClass("col-12 col-md-1 hour");
+
+    
+    var descriptionEl = $("<div>")
+    descriptionEl.addClass("col-10 col-md-10 description p-0");
+
+        var descriptionText = $("<textarea>");
+        descriptionText.addClass("user-input");
+
+        descriptionEl.append(descriptionText);
+        descriptionText.attr("id", timeBlockEl.row);
+
+    if (timeBlockEl.time < moment().format("HH")) {
+        descriptionEl.addClass("past");
+
+    } else if (timeBlockEl.time === moment().format("HH")){
+        descriptionEl.addClass("future");
+
+    }else if (timeBlockEl.time > moment().format("HH")){
+        descriptionEl.addClass("future");
+    }
+
+
+
+    
 });
